1bd29323d67302120d6bc0307ce95b249bf2d515
dogusdicle <dogus.dicle@maksisoft.com>
2020-08-11 13:38:37 +0300

    backend revisioned for access control

--- server/controllers/access.controller.js
+++ server/controllers/access.controller.js
@@ -5,113 +5,52 @@ import extend from 'lodash/extend';
 import dbErrorHandler from '../helpers/dbErrorHandler';
 
 const access = (req, res) => {
-  const { personId, doorId } = req.body;
+  let door = req.door;
+  let person = req.person;
+
+  let newLOg = new Log({
+    'personId': person.id,
+    'doorId': door.id,
+    'doorName': door.name,
+    'personFullName': person.fullname,
+    'isPermitted': false
+  })
+
+  
+  if(person.doors.includes(door.id)){
+    newLOg.isPermitted = true;
+  }
 
 
-  Person.findOne({'personId': personId})
-    .then( person => {
-      let doorObjectId = null;
-      return Door.findOne({'doorId': doorId})
-        .then( door => {
-          if(!door)
-            return res.status(406).json({
-              'error': 'Door not found'
-            })
-          doorObjectId = door.id;
-          const doorName = door.name;
-          const personFullName = person.fullname;
-          return {doorObjectId, door, person, doorName, personFullName}
+  newLOg.save()
+    .then( async newLog => {
+      door.logs.push(newLog);
+      person.logs.push(newLog);
+
+      try {
+        await door.save();
+        await person.save();
+      } catch (err) {
+        return res.status(400).json({
+          'error': err.message
         })
-        .catch( err => res.status(400).json({
-          'error': "Door not able to be retrived"
-        }))
-    
-    })
-    .then( ({doorObjectId, person, door, doorName, personFullName}) => {
-
-      const doorObjectIdList = person.get('doors');
-
-      let result = false;
-
-
-      doorObjectIdList.forEach( doorObjectIdItem => {
-        // In fact, we compare string vs object, since we use '=='
-        // operator, javascript engine introduce at this point
-        // 'type coercion' to the stage.
-        if(doorObjectId == doorObjectIdItem ){
-          result = true;
-        }
-      });
-
-
-      if(result){
-        console.log('he')
-        const newLog = new Log({
-          'personId': personId,
-          'doorId': doorId,
-          'doorName': doorName,
-          'personFullName': personFullName,
-          'isPermitted': true
-        })
-        newLog.save()
-          .then( async newLog => {
-            door.logs.push(newLog);
-            person.logs.push(newLog);
-
-            try {
-              await door.save();
-              await person.save();
-            } catch (err) {
-              return res.status(400).json({
-                'error': err.message
-              })
-            }
-
-            return res.status(200).json({
-              'message': 'permitted'
-            })
-          })
-          .catch( err => res.status(400).json({
-              'error': dbErrorHandler.getErrorMessage(err)
-          }));
-
-      } else {
-        const newLog = new Log({
-          'personId': personId,
-          'doorId': doorId,
-          'doorName': doorName,
-          'personFullName': personFullName,
-          'isPermitted': false
-        });
-
-
-        newLog.save()
-          .then( async newLog => {
-            door.logs.push(newLog);
-            person.logs.push(newLog);
-
-            try {
-              await door.save();
-              await person.save();
-            } catch (err) {
-              return res.status(400).json({
-                'error': err.message
-              })
-            }
-
-            return res.status(200).json({
-              'message': 'denied'
-            })
-          })
-          .catch( err => res.status(400).json({
-              'error': dbErrorHandler.getErrorMessage(err)
-          }));
       }
+      if(newLOg.isPermitted){
+        return res.status(200).json({
+          'message': 'permitted'
+        });
+      } else {
+        return res.status(200).json({
+          'message': 'denied'
+        });
+      }
+
 
     })
     .catch( err => res.status(400).json({
-      'error': "Person not able to be retrived"
-    }))
+        'error': dbErrorHandler.getErrorMessage(err)
+    }));
+
 }
 
 export default {
--- server/routes/access.route.js
+++ server/routes/access.route.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import accessCtrl from '../controllers/access.controller';
+import personCtrl from '../controllers/person.controller';
+import doorCtrl from '../controllers/door.controller';
 
 const router = express.Router();
 
-router.route('/api/access')
-    .post(accessCtrl.access)
+router.route('/api/access/:doorId/:personId')
+    .post(accessCtrl.access);
 
 
+router.param('personId', personCtrl.personByID);
+router.param('doorId', doorCtrl.doorByID);
 
 export default router;
